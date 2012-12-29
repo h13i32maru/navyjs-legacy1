edc41111d11589725e3647ab01b077e41d85fc91
h13i32maru <h13i32maru@gmail.com>
2012-12-29 19:44:21 +0900

    config一覧の取得とファイルの内容を表示するようにした

--- builder/www/js/builder.js
+++ builder/www/js/builder.js
@@ -1,19 +1,65 @@
+var read = function(path, callback) {
+    var params = {path: path};
+    $.getJSON('/data', params, callback);
+}
+
+var format = function(str, arg) {
+    console.log(arg);
+    for (var i = 0; i < arg.length; i++) {
+        str = str.replace('%s', arg[i]);
+    }
+    return str;
+}
+
 var Header = {
+    $el: null,
     projects: ko.observableArray(),
+    selectedProject: ko.observable(),
 
     init: function(){
-        ko.applyBindings(Header, $('.n-header')[0]);
+        this.$el = $('.n-header');
+        ko.applyBindings(this, this.$el[0]);
 
-        var self = this;
-        var params = {path: '/'};
-        $.getJSON('/data', params, function(data){
-            console.log(arguments);
-            self.projects(data);
+        read('/', function(data){
+            this.projects(data);
         });
+    }
+};
+
+var Config = {
+    $el: null,
+    files: ko.observableArray([]),
+    project: null,
+    
+    init: function(){
+        this.$el = $('.n-config');
+        ko.applyBindings(this, this.$el[0]);
+        ko.computed(this.onChangeProject.bind(this));
+    },
+
+    onChangeProject: function(){
+        var project = Header.selectedProject();
+        this.project = project;
+        if (!project) {
+            return;
+        }
+
+        var path = format('/%s/config', [project]);
+        read(path, function(data){
+            this.files(data);
+        }.bind(this));
+    },
+
+    readFile: function(data, ev){
+        var path = format('/%s/config/%s', [this.project, data]);
+        read(path, function(data){
+            this.$el.find('textarea').val(data.content);
+        }.bind(this));
     }
 };
 
 
 $(function(){
     Header.init();
+    Config.init();
 });
