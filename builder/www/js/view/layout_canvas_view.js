c94a3e1e5d0e8f2eba5f567d91de9c7549dd5fd4
h13i32maru <h13i32maru@gmail.com>
2013-01-05 23:30:24 +0900

    pythonで作っていた時の古いコードを削除

--- builder/www/js/view/layout_canvas_view.js
+++
@@ -1,8 +0,0 @@
-var LayoutCanvasView = Backbone.View.extend({
-    el: '#bs-layout-canvas',
-    initialize: function() {
-        //iPhone4Sのサイズを基本としている
-        var width = 640 * this.$el.height() / 960;
-        this.$el.width(width);
-    }
-});
