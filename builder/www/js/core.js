875bfc8e7f225692e4e66110a30bd32b176b2a0e
h13i32maru <h13i32maru@gmail.com>
2013-01-12 17:52:35 +0900

    ファイルを個別に変更管理できるようにした

--- builder/www/js/core.js
+++ builder/www/js/core.js
@@ -1,3 +1,97 @@
+Builder.Core2 = nClass({
+    CLASS: 'Core',
+    type: '',
+    project: null,
+
+    koFile: null,
+    koFiles: null,
+    koVisible: null,
+
+    initialize: function() {
+        this.initObservable();
+    },
+
+    initObservable: function() {
+        this.koFile = ko.observable();
+        this.koFiles = ko.observableArray([]);
+        this.koVisible = ko.observable(false);
+        ko.computed(this.onChangeProject.bind(this));
+    },
+
+    onChangeProject: function(){
+        var project = Builder.Header.selectedProject();
+        this.project = project;
+        if (!project) {
+            return;
+        }
+
+        var path = Builder.Util.format('/%s/%s', [project, this.type]);
+        Builder.Util.read(path, this.onReadFilenames.bind(this));
+    },
+
+    onReadFilenames: function(filenames) {
+        var files = [];
+        var file;
+        for (var i = 0; i < filenames.length; i++) {
+            file = new Builder.File(filenames[i], null);
+            files.push(file);
+        }
+        this.koFiles(files);
+    },
+
+    select: function(file) {
+        var files = this.koFiles();
+        for (var i = 0; i < files.length; i++) {
+            files[i].selected(false);
+        }
+
+        file.selected(true);
+        this.koFile(file);
+    },
+
+    findFile: function(filename) {
+        var files = this.koFiles();
+        for (var i = 0; i < files.length; i++) {
+            if (files[i].getFilename() === filename) {
+                return files[i];
+            }
+        }
+        return null;
+    },
+
+    readFile: function(file){
+        this.select(file);
+        
+        if (file.changed()) {
+            var text = file.getText();
+            this.onReadFile({content: text});
+        } else {
+            var path = Builder.Util.format('/%s/%s/%s', [this.project, this.type, file.getFilename()]);
+            Builder.Util.read(path, this.onReadFile.bind(this));
+        }
+    },
+
+    onReadFile: function(data) {
+        var text = data.content;
+        this.koFile().setText(text);
+    },
+
+    save: function(key, ev) {
+        var filename = ev.$trigger.text();
+        var file = this.findFile(filename);
+        if (file.getText() === null) {
+            return;
+        }
+
+        var text = file.getText();
+        var path = Builder.Util.format('/%s/%s/%s', [this.project, this.type, filename]);
+        Builder.Util.write(path, text, this.onSaveFile.bind(this, file));
+    },
+
+    onSaveFile: function(file) {
+        file.changed(false);
+    }
+});
 Builder.Core = nClass({
     CLASS: 'Core',
     target: '',
--- builder/www/js/index.js
+++ builder/www/js/index.js
@@ -5,8 +5,17 @@ $(function(){
     Navy.App.wakeup();
 
     Builder.Header.initialize();
-    Builder.Config.initialize();
     Builder.Layout.initialize();
     Builder.Code.initialize();
     Builder.Image.initialize();
+
+    //Config
+    Builder.Config.initialize();
+    $.contextMenu({
+        selector: '.n-config .n-filelist li',
+        items: {
+            save: {name: 'Save', callback: function(key, ev){ Builder.Config.save(key, ev); }}
+        }
+    });
+    ko.applyBindings(Builder.Config, $('.n-config')[0]);
 });
